8f56b3d1e912c4314ee976408f0a67f259f61456
maksOmy <antipov.maks00@mail.ru>
2020-06-25 15:18:43 +0400

    final project

--- src/prime.js
+++ src/prime.js
@@ -4,4 +4,4 @@ import { getRandomNum } from './even.js';
 export const getRandomQuestion = () => getRandomNum(1, 50);
 export const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 export const getUserAnswer = () => readlineSync.question('Your answer: ', { limit: ['yes', 'no'] });
-export const checkCorrectAnswer = (num) => num % 2 === 0 || num % 3 === 0 ? 'no' : 'yes';;
+export const checkCorrectAnswer = (num) => num % 2 === 0 || num % 3 === 0 ? 'no' : 'yes';
